bc8f1225cf5e1e5d1e27e74f823044bf4b240d38
Dani-lin-lp <dani.linux.97@gmail.com>
2022-09-27 17:12:45 +0200

    Add drawer for adding new lists

---
+++ src/components/AddList.vue
@@ -0,0 +1,77 @@
+<script>
+export default {
+  name: "AddList",
+  data() {
+    return {
+      list: {
+        title: "",
+        tag: false,
+        reminder: false,
+      },
+      todos: [],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="drawer drawer-mobile">
+    <input id="add-list-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col items-center justify-center"></div>
+    <div class="drawer-side w-1/2">
+      <label for="add-list-drawer" class="drawer-overlay"></label>
+      <div class="m-4 p-4 flex flex-col justify-around items-center">
+        <label class="text-2xl text-primary">
+          TITLE:
+          <input
+            type="text"
+            v-model="list.title"
+            placeholder="Finish Paper Work"
+            class="text-success border-b border-success"
+          />
+        </label>
+        <label class="text-2xl text-primary">
+          REMINDER:
+          <input
+            type="datetime-local"
+            v-model="list.reminder"
+            class="text-success border-b border-success"
+          />
+        </label>
+        <label class="text-2xl text-primary">
+          TAG:
+          <select v-model="list.tag" class="select select-success w-full max-w-xs select-lg">
+            <option value="false">PICK A TAG</option>
+            <option value="home">HOME</option>
+            <option value="school">SCHOOL</option>
+            <option value="work">WORK</option>
+          </select>
+        </label>
+        <div class="flex flex-col justify-center items-center">
+          <h3>TODOS</h3>
+          <label>
+            <button class="btn btn-primary btn-sm">ADD</button>
+            <input type="text" class="text-success border-b border-success px-2"/>
+          </label>
+
+          <ul
+            tabindex="0"
+            class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            v-if="todos.length > 0"
+          >
+            <li v-for="todo in todos" :key="todo.id">
+              {{todo.content}}
+            </li>
+          </ul>
+        </div>
+        <button v-if="list.title" class="btn btn-success">SAVE</button>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="css" scoped>
+input {
+  background-color: transparent;
+  outline: none;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,32 +1,29 @@
 <script>
 import SpinnerModal from "../components/SpinnerModal.vue";
+import AddList from "../components/AddList.vue"
 export default {
   data() {
     return {
-      showSpinner: true,
-      posts: false
+      showSpinner: false,
     };
   },
   components: {
     SpinnerModal,
+    AddList
   },
   methods: {
-    async getPosts(){
-      const data = await allPosts()
-      if(data.status){
-        this.posts = data.posts
-        this.showSpinner = false
-      }
-    }
+
   },
   mounted() {
-    this.getPosts()
   },
 };
 </script>
 <template>
   <main class="my-6 flex justify-center items-center flex-col">
     <spinner-modal :show="showSpinner"></spinner-modal>
-    <h1>hi</h1>
+    <label for="add-list-drawer" class="btn btn-primary drawer-button lg:hidden">ADD LIST</label>
+
+    <add-list></add-list>
+
   </main>
 </template>
