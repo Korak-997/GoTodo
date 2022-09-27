cae13eddda6b2b918ce3cccf5a470a340716f2b9
Dani-lin-lp <dani.linux.97@gmail.com>
2022-09-27 06:50:34 +0200

    Add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,28 @@ module.exports = {
   content: ["./src/**/*.{vue,js,ts}"],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["black"],
+    themes: [
+      {
+        mytheme: {
+          primary: "#4ade80",
+
+          secondary: "#0284c7",
+
+          accent: "#ff8303",
+
+          neutral: "#191D24",
+
+          "base-100": "#141414",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
   },
 };
